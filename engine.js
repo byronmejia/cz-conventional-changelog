86b52391f9c0acc1edf1fe45ea1fbc4728f24ad4
Byron Mejia <hello@byronis.me>
2018-12-20 11:42:13 +1000

    feat(a): b
    asdf

--- engine.js
+++ engine.js
@@ -1,10 +1,6 @@
-"format cjs";
-
-var wrap = require('word-wrap');
-var map = require('lodash.map');
-var longest = require('longest');
-var rightPad = require('right-pad');
-const questionGenerator = require('./questions');
+const wrap = require("word-wrap");
+const questionGenerator = require("./questions");
+const autocomplete = require("inquirer-autocomplete-prompt");
 
 var filter = function(array) {
   return array.filter(function(x) {
@@ -12,75 +8,58 @@ var filter = function(array) {
   });
 };
 
-// This can be any kind of SystemJS compatible module.
-// We use Commonjs here, but ES6 or AMD would do just
-// fine.
-module.exports = function (options) {
-
-  var types = options.types;
-
-  var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function (type, key) {
-    return {
-      name: rightPad(key + ':', length) + ' ' + type.description,
-      value: key
-    };
-  });
+module.exports = options => {
+  const types = options.types;
+  const length = Math.max(...Object.keys(types).map(key => key.length)) + 1;
+  const choices = Object.entries(types).map(([key, value]) => ({
+    name: `${`${key}:`.padEnd(length, " ")} ${value.description}`,
+    value: key
+  }));
 
   return {
-    // When a user runs `git cz`, prompter will
-    // be executed. We pass you cz, which currently
-    // is just an instance of inquirer.js. Using
-    // this you can ask questions and get answers.
-    //
-    // The commit callback should be executed when
-    // you're ready to send back a commit template
-    // to git.
-    //
-    // By default, we'll de-indent your commit
-    // template and will keep empty lines.
-    prompter: function(cz, commit) {
-      // Let's ask some questions of the user
-      // so that we can populate our commit
-      // template.
-      //
-      // See inquirer.js docs for specifics.
-      // You can also opt to use another input
-      // collection library if you prefer.
-      cz.prompt.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
-      cz.prompt(
-        questionGenerator(options, choices)
-      ).then(function(answers) {
+    prompter: (cz, commit) => {
+      cz.prompt.registerPrompt("autocomplete", autocomplete);
+      cz.prompt(questionGenerator(options, choices)).then(answers => {
+        const maxLineWidth = 100;
 
-        var maxLineWidth = 100;
-
-        var wrapOptions = {
+        const wrapOptions = {
           trim: true,
-          newline: '\n',
-          indent:'',
+          newline: "\n",
+          indent: "",
           width: maxLineWidth
         };
 
         // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? '(' + answers.scope.trim() + ')' : '';
+        const scope = answers.scope.trim();
+        scope = scope ? "(" + answers.scope.trim() + ")" : "";
 
         // Hard limit this line
-        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth).toLocaleLowerCase();
+        const head = (answers.type + scope + ": " + answers.subject.trim())
+          .slice(0, maxLineWidth)
+          .toLocaleLowerCase();
 
         // Wrap these lines at 100 characters
-        var body = wrap(answers.body, wrapOptions);
+        const body = wrap(answers.body, wrapOptions);
 
         // Apply breaking change prefix, removing it if already present
-        var breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
+        var breaking = answers.breaking ? answers.breaking.trim() : "";
+        breaking = breaking
+          ? "BREAKING CHANGE: " + breaking.replace(/^BREAKING CHANGE: /, "")
+          : "";
         breaking = wrap(breaking, wrapOptions);
 
-        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
+        const issues = answers.issues ? wrap(answers.issues, wrapOptions) : "";
 
-        var footer = filter([ breaking, issues ]).join('\n\n');
+        const footer = filter([breaking, issues]).join("\n\n");
+        commit(`
+        ${head}
 
-        commit(head + '\n\n' + body + '\n\n' + footer);
+
+        ${body}
+
+
+        ${footer}
+        `);
       });
     }
   };
